58604799e781583d9fd0010a7c6d9ab7ae89b64b
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-17 01:32:49 -0500

    Closes #2

--- src/main/java/recraft/cpc/common/entity/ai/ChildHunt.java
+++ src/main/java/recraft/cpc/common/entity/ai/ChildHunt.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.pathfinding.PathPoint;
 import net.minecraft.util.MathHelper;
@@ -33,54 +35,48 @@ public class ChildHunt extends EntityAIBase {
 
 	private int failedPathFindingPenalty;
 
-	public ChildHunt(EntityCreature par1EntityCreature, Class<? extends EntityAgeable> par2Class, double par3, boolean par5)
-	{
-		this(par1EntityCreature, par3, par5);
-		this.classTarget = par2Class;
+	public ChildHunt(EntityCreature creature,Class<? extends EntityAgeable> target,
+                     double speed, boolean memory) {
+		this(creature, speed, memory);
+		classTarget = target;
 	}
 
-	public ChildHunt(EntityCreature par1EntityCreature, double par2, boolean par4)
-	{
-		this.attacker = par1EntityCreature;
-		this.worldObj = par1EntityCreature.worldObj;
-		this.speedTowardsTarget = par2;
-		this.longMemory = par4;
-		this.setMutexBits(3);
+	public ChildHunt(EntityCreature creature, double speed, boolean memory) {
+		attacker = creature;
+		worldObj = creature.worldObj;
+		speedTowardsTarget = speed;
+		longMemory = memory;
+		setMutexBits(3);
 	}
 
 	/**
 	 * Returns whether the EntityAIBase should begin execution.
 	 */
-	public boolean shouldExecute()
-	{
-		EntityAgeable entityAgeable = (EntityAgeable) this.attacker.getAttackTarget();
+	public boolean shouldExecute() {
+        EntityLivingBase entity = attacker.getAttackTarget();
 
-		if (entityAgeable == null)
-		{
+        if (entity == null) {
+            return false;
+        }
+        else if (!(entity instanceof EntityVillager)) {
+            return false;
+        }
+		else if (!entity.isEntityAlive()) {
 			return false;
 		}
-		else if (!entityAgeable.isEntityAlive())
-		{
+		else if (classTarget != null && !classTarget.isAssignableFrom(entity.getClass())) {
 			return false;
 		}
-		else if (this.classTarget != null && !this.classTarget.isAssignableFrom(entityAgeable.getClass()))
-		{
+		else if(!entity.isChild()) {
 			return false;
 		}
-		else if(!entityAgeable.isChild())
-		{
-			return false;
-		}
-		else
-		{
-			if (-- this.field_75445_i <= 0)
-			{
-				this.entityPathEntity = this.attacker.getNavigator().getPathToEntityLiving(entityAgeable);
-				this.field_75445_i = 4 + this.attacker.getRNG().nextInt(7);
-				return this.entityPathEntity != null;
+		else {
+			if ( --field_75445_i <= 0) {
+				entityPathEntity = attacker.getNavigator().getPathToEntityLiving(entity);
+				field_75445_i = 4 + attacker.getRNG().nextInt(7);
+				return entityPathEntity != null;
 			}
-			else
-			{
+			else {
 				return true;
 			}
 		}
@@ -90,14 +86,18 @@ public class ChildHunt extends EntityAIBase {
 	 * Returns whether an in-progress EntityAIBase should continue executing
 	 */
 	public boolean continueExecuting() {
-		if (this.attacker.getAttackTarget() != null) {
-			if (this.attacker.getAttackTarget().isEntityAlive()) {
-				if (!this.longMemory) {
-					if (!this.attacker.getNavigator().noPath()) {
+		if (attacker.getAttackTarget() != null) {
+			if (attacker.getAttackTarget().isEntityAlive()) {
+				if (!longMemory) {
+					if (!attacker.getNavigator().noPath()) {
 						return true;
 					}
-				} else {
-					if (this.attacker.isWithinHomeDistance(MathHelper.floor_double(this.attacker.getAttackTarget().posX), MathHelper.floor_double(this.attacker.getAttackTarget().posY), MathHelper.floor_double(this.attacker.getAttackTarget().posZ))) {
+				}
+                else {
+					if (attacker.isWithinHomeDistance(
+                            MathHelper.floor_double(attacker.getAttackTarget().posX),
+                            MathHelper.floor_double(attacker.getAttackTarget().posY),
+                            MathHelper.floor_double(attacker.getAttackTarget().posZ))) {
 						return true;
 					}
 				}
@@ -110,62 +110,56 @@ public class ChildHunt extends EntityAIBase {
 	 * Execute a one shot task or start executing a continuous task
 	 */
 	public void startExecuting() {
-		this.attacker.getNavigator().setPath(this.entityPathEntity, this.speedTowardsTarget);
-		this.field_75445_i = 0;
+		attacker.getNavigator().setPath(entityPathEntity, speedTowardsTarget);
+		field_75445_i = 0;
 	}
 
 	/**
 	 * Resets the task
 	 */
 	public void resetTask() {
-		this.attacker.getNavigator().clearPathEntity();
+		attacker.getNavigator().clearPathEntity();
 	}
 
 	/**
 	 * Updates the task
 	 */
-	public void updateTask()
-	{
-		EntityLivingBase entitylivingbase = this.attacker.getAttackTarget();
-		this.attacker.getLookHelper().setLookPositionWithEntity(entitylivingbase, 30.0F, 30.0F);
+	public void updateTask() {
+		EntityLivingBase entity = attacker.getAttackTarget();
+		attacker.getLookHelper().setLookPositionWithEntity(entity, 30.0F, 30.0F);
 
-		if ((this.longMemory || this.attacker.getEntitySenses().canSee(entitylivingbase)) && --this.field_75445_i <= 0)
-		{
-			this.field_75445_i = failedPathFindingPenalty + 4 + this.attacker.getRNG().nextInt(7);
-			this.attacker.getNavigator().tryMoveToEntityLiving(entitylivingbase, this.speedTowardsTarget);
-			if (this.attacker.getNavigator().getPath() != null)
-			{
-				PathPoint finalPathPoint = this.attacker.getNavigator().getPath().getFinalPathPoint();
-				if (finalPathPoint != null && entitylivingbase.getDistanceSq(finalPathPoint.xCoord, finalPathPoint.yCoord, finalPathPoint.zCoord) < 1)
+		if ((longMemory || attacker.getEntitySenses().canSee(entity)) && --field_75445_i <= 0) {
+			field_75445_i = failedPathFindingPenalty + 4 + attacker.getRNG().nextInt(7);
+			attacker.getNavigator().tryMoveToEntityLiving(entity, speedTowardsTarget);
+			if (attacker.getNavigator().getPath() != null) {
+				PathPoint finalPathPoint = attacker.getNavigator().getPath().getFinalPathPoint();
+				if (finalPathPoint != null && entity.getDistanceSq(
+                        finalPathPoint.xCoord,
+                        finalPathPoint.yCoord,
+                        finalPathPoint.zCoord) < 1)
 				{
 					failedPathFindingPenalty = 0;
 				}
-				else
-				{
+				else {
 					failedPathFindingPenalty += 10;
 				}
 			}
-			else
-			{
+			else {
 				failedPathFindingPenalty += 10;
 			}
 		}
 
-		this.attackTick = Math.max(this.attackTick - 1, 0);
-		double d0 = (double)(this.attacker.width * 2.0F * this.attacker.width * 2.0F + entitylivingbase.width);
+		attackTick = Math.max(attackTick - 1, 0);
+		double d0 = (double)(attacker.width * 2.0F * attacker.width * 2.0F + entity.width);
 
-		if (this.attacker.getDistanceSq(entitylivingbase.posX, entitylivingbase.boundingBox.minY, entitylivingbase.posZ) <= d0)
-		{
-			if (this.attackTick <= 0)
-			{
+		if (attacker.getDistanceSq(entity.posX, entity.boundingBox.minY, entity.posZ) <= d0) {
+			if (this.attackTick <= 0) {
 				this.attackTick = 20;
 
-				if (this.attacker.getHeldItem() != null)
-				{
-					this.attacker.swingItem();
+				if (attacker.getHeldItem() != null) {
+					attacker.swingItem();
 				}
-
-				this.attacker.attackEntityAsMob(entitylivingbase);
+				attacker.attackEntityAsMob(entity);
 			}
 		}
 	}
